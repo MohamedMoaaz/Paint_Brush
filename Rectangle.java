04a2d2674e44d6c9abb2495d2095a11baf226b28
Mohamed Moaaz <Mohamedmoaaz646@gmail.com>
2025-01-19 22:10:13 +0200

    adding Rectangle Class

---
+++ Rectangle.java
@@ -0,0 +1,14 @@
+import java.awt.Graphics;
+public class Rectangle implements Shape {
+    private int x, y, width, height;
+    public Rectangle(int x, int y, int width, int height) {
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
+    }
+    @Override
+    public void draw(Graphics g) {
+        g.drawRect(x, y, width, height);
+    }
+}
