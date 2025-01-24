0679a5ef7e44969e406a9c58e15a5f8e045ba3e6
Mohamed Moaaz <Mohamedmoaaz646@gmail.com>
2025-01-24 11:57:03 +0200

    update class oval

--- Oval.java
+++ Oval.java
@@ -2,11 +2,34 @@
 import java.awt.Color;
 import java.awt.Graphics;
 
+/**
+ * A class representing an oval shape.
+ * This class extends the abstract `Shape` class and implements the `draw` method
+ * to render an oval on a graphics context. The oval can be either filled or outlined,
+ * depending on the `filled` property.
+ */
 public class Oval extends Shape {
+
+    /**
+     * Constructor for the Oval class.
+     *
+     * @param x1     The x-coordinate of the first point of the oval's bounding box.
+     * @param y1     The y-coordinate of the first point of the oval's bounding box.
+     * @param x2     The x-coordinate of the second point of the oval's bounding box.
+     * @param y2     The y-coordinate of the second point of the oval's bounding box.
+     * @param color  The color of the oval.
+     * @param filled Whether the oval is filled (true) or outlined (false).
+     */
     public Oval(int x1, int y1, int x2, int y2, Color color, boolean filled) {
         super(x1, y1, x2, y2, color, filled);
     }
 
+    /**
+     * Draws the oval on the specified graphics context.
+     * If the oval is filled, it uses the `fillOval` method; otherwise, it uses the `drawOval` method.
+     *
+     * @param g The Graphics object used to draw the oval.
+     */
     @Override
     public void draw(Graphics g) {
         g.setColor(color);
